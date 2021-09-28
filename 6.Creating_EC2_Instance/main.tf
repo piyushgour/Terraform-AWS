7915e13688b7b1b17523d116b772075c824daed1
Piyush <piyush.gour93@gmail.com>
2021-09-28 22:11:59 +0530

    adding more example

--- 6.Creating_EC2_Instance/main.tf
+++ 6.Creating_EC2_Instance/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "test1" {
 }
 
 
-## EC2 with Hardcode Values 
+## EC2 with Hardcode Values and Tags
 
 resource "aws_instance" "test2" {
   ami                    = "ami-0ff8a91507f77f867"
@@ -49,3 +49,31 @@ resource "aws_instance" "test3" {
   }
 }
 
+# EC2 with additional EBS volume and Encryption Enable
+resource "aws_instance" "test3" {
+  ami                    = "ami-0ff8a91507f77f867"
+  instance_type          = "t2.micro"
+  key_name               = "test3"
+  subnet_id              = "sub-123456788"
+  vpc_security_group_ids = ["sg-123456", "sg-987654"]
+  ebs_optimized          = false
+  monitoring             = false
+  iam_instance_profile = "Profile-Name"
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+    encrypted = true
+    delete_on_termination = true
+
+  }
+
+  ebs_block_device {
+      device_name = "/dev/sdf"
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted = true
+    delete_on_termination = false
+
+  }  
+}
