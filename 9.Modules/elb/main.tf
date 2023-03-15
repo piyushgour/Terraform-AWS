8b8aea6943445531af784d21fcf772553cc30b69
Piyush Gour <piyush.gour@hitachivantara.com>
2023-03-15 10:37:45 +0530

    adding alb resources

--- 9.Modules/elb/main.tf
+++ 9.Modules/elb/main.tf
@@ -1,3 +1,173 @@
 provider "aws" {
   region = "us-east-1"
 }
+# resource "aws_lb_target_group" "alb-example" {
+#   name        = "tf-example-lb-alb-tg"
+#   target_type = "ip"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = "vpc-d6d2d1ac"
+# }
+
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.test.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb-example.arn
+#   }
+# }
+
+# resource "aws_lb_target_group_attachment" "test" {
+#   target_group_arn = aws_lb_target_group.alb-example.arn
+#   target_id        = "172.31.1.0"
+#   port             = 80
+# }
+# resource "aws_lb" "test" {
+#   name               = "test-lb-tf"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = ["sg-0e315973aa76f0a87"]
+#   subnets            = ["subnet-0ea52343","subnet-b58f7294"]
+
+#   enable_deletion_protection = false
+#   # access_logs {
+#   #   bucket  = aws_s3_bucket.lb_logs.id
+#   #   prefix  = "test-lb"
+#   #   enabled = true
+#   # }
+
+#   tags = {
+#     Environment = "terraform-test"
+#   }
+# }
+
+
+
+####################################
+###     New Module
+####################################
+
+locals {
+  create_lb = var.create_lb
+}
+resource "aws_lb" "this" {
+  #count = local.create_lb ? 1 : 0
+
+  name        = var.name
+
+  load_balancer_type = var.load_balancer_type
+  internal           = var.internal
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+
+  idle_timeout                     = var.idle_timeout
+  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
+  enable_deletion_protection       = var.enable_deletion_protection
+  enable_http2                     = var.enable_http2
+  ip_address_type                  = var.ip_address_type
+  drop_invalid_header_fields       = var.drop_invalid_header_fields
+  preserve_host_header             = var.preserve_host_header
+  enable_waf_fail_open             = var.enable_waf_fail_open
+  desync_mitigation_mode           = var.desync_mitigation_mode
+
+  # access_logs {
+  #   bucket  = aws_s3_bucket.lb_logs.id
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
+
+  tags = merge(
+    {
+      Name = (var.name != null) ? var.name : var.name_prefix
+    },
+    var.tags,
+    var.lb_tags,
+  )
+  timeouts {
+    create = var.load_balancer_create_timeout
+    update = var.load_balancer_update_timeout
+    delete = var.load_balancer_delete_timeout
+  }
+}
+
+
+resource "aws_lb_target_group" "alb-example" {
+  name        = "tf-example-lb-alb-tg"
+  target_type = var.target_type
+  port        = 80
+  protocol    = upper(var.protocol)
+  protocol_version = upper(var.protocol_version)
+  vpc_id      = var.vpc_id
+
+  load_balancing_algorithm_type = var.load_balancing_algorithm_type
+
+  health_check {
+    path = var.health_check_path
+    port = var.health_check_port
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
+
+  stickiness {
+    cookie_duration = 3600
+    enabled = true
+    type = "lb_cookie"
+  }
+
+    tags = merge(
+    var.tags,
+    var.target_group_tags,
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+
+}
+
+# Create a Listener 
+resource "aws_alb_listener" "my-alb-listener" {
+  depends_on = [aws_lb_target_group.alb-example]
+  #count = local.create_lb ? length(var.https_listeners) : 0
+  default_action {
+    target_group_arn =  aws_lb_target_group.alb-example.arn
+    type = "forward"
+  }
+  load_balancer_arn = aws_lb.this.arn
+  port = 80
+  protocol = "HTTP"
+}
+
+
+# Create Listener Rules
+resource "aws_alb_listener_rule" "rule-1" {
+  depends_on = [
+    aws_lb_target_group.alb-example
+  ]
+  action {
+    target_group_arn =  aws_lb_target_group.alb-example.arn
+    type = "forward"
+  }
+  condition {
+      path_pattern {
+      values = ["/*"]
+    }
+  }
+
+  listener_arn = aws_alb_listener.my-alb-listener.arn
+  priority = 100
+}
+
+
+# resource "aws_lb_target_group_attachment" "alb_taget_group_attachment" {
+#   target_group_arn = aws_lb_target_group.alb-example
+#   target_id        = aws_lb.this.id
+#   port             = 80
+# }
+
---
+++ 9.Modules/elb/variables.tf
@@ -0,0 +1,205 @@
+variable "alb_name" {
+  type = string
+  default = ""
+  description = "Alb Name"
+}
+
+#############################################
+###         New Variables
+#############################################
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "lb_tags" {
+  description = "A map of lb_tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "target_group_tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_lb" {
+  description = "Controls if the Load Balancer should be created"
+  type        = bool
+  default     = true
+}
+
+variable "name" {
+  description = "The resource name and Name tag of the load balancer."
+  type        = string
+  default     = null
+}
+
+variable "name_prefix" {
+  description = "The resource name prefix and Name tag of the load balancer. Cannot be longer than 6 characters"
+  type        = string
+  default     = null
+}
+
+variable "load_balancer_type" {
+  description = "The type of load balancer to create. Possible values are application or network."
+  type        = string
+  default     = "application"
+}
+
+variable "internal" {
+  description = "Boolean determining if the load balancer is internal or externally facing."
+  type        = bool
+  default     = false
+}
+
+variable "access_logs" {
+  description = "Map containing access logging configuration for load balancer."
+  type        = map(string)
+  default     = {}
+}
+
+variable "security_groups" {
+  description = "The security groups to attach to the load balancer. e.g. [\"sg-edcd9784\",\"sg-edcd9785\"]"
+  type        = list(string)
+  default     = []
+}
+
+variable "subnets" {
+  description = "A list of subnets to associate with the load balancer. e.g. ['subnet-1a2b3c4d','subnet-1a2b3c4e','subnet-1a2b3c4f']"
+  type        = list(string)
+  default     = null
+}
+
+variable "idle_timeout" {
+  description = "The time in seconds that the connection is allowed to be idle."
+  type        = number
+  default     = 60
+}
+
+variable "enable_cross_zone_load_balancing" {
+  description = "Indicates whether cross zone load balancing should be enabled in application load balancers."
+  type        = bool
+  default     = false
+}
+
+variable "enable_deletion_protection" {
+  description = "If true, deletion of the load balancer will be disabled via the AWS API. This will prevent Terraform from deleting the load balancer. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "enable_http2" {
+  description = "Indicates whether HTTP/2 is enabled in application load balancers."
+  type        = bool
+  default     = true
+}
+
+variable "ip_address_type" {
+  description = "The type of IP addresses used by the subnets for your load balancer. The possible values are ipv4 and dualstack."
+  type        = string
+  default     = "ipv4"
+}
+
+variable "drop_invalid_header_fields" {
+  description = "Indicates whether invalid header fields are dropped in application load balancers. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "preserve_host_header" {
+  description = "Indicates whether Host header should be preserve and forward to targets without any change. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "enable_waf_fail_open" {
+  description = "Indicates whether to route requests to targets if lb fails to forward the request to AWS WAF"
+  type        = bool
+  default     = false
+}
+
+variable "desync_mitigation_mode" {
+  description = "Determines how the load balancer handles requests that might pose a security risk to an application due to HTTP desync."
+  type        = string
+  default     = "defensive"
+}
+
+
+variable "extra_ssl_certs" {
+  description = "A list of maps describing any extra SSL certificates to apply to the HTTPS listeners. Required key/values: certificate_arn, https_listener_index (the index of the listener within https_listeners which the cert applies toward)."
+  type        = list(map(string))
+  default     = []
+}
+
+
+
+variable "load_balancer_create_timeout" {
+  description = "Timeout value when creating the ALB."
+  type        = string
+  default     = "10m"
+}
+
+variable "load_balancer_delete_timeout" {
+  description = "Timeout value when deleting the ALB."
+  type        = string
+  default     = "10m"
+}
+
+variable "load_balancer_update_timeout" {
+  description = "Timeout value when updating the ALB."
+  type        = string
+  default     = "10m"
+}
+
+variable "target_groups" {
+  description = "A list of maps containing key/value pairs that define the target groups to be created. Order of these maps is important and the index of these are to be referenced in listener definitions. Required key/values: name, backend_protocol, backend_port"
+  type        = any
+  default     = []
+}
+
+variable "vpc_id" {
+  description = "VPC id where the load balancer and other resources will be deployed."
+  type        = string
+  default     = null
+}
+
+variable "target_type" {
+  description = "Target type allowed valuse for ALB is instance or ip."
+  type        = string
+  default     = null
+}
+
+variable "load_balancing_algorithm_type" {
+  description = "load_balancing_algorithm_type allowed valuse for ALB is round_robin or least_outstanding_requests."
+  type        = string
+  default     = "round_robin"
+}
+
+variable "protocol" {
+  description = "Protocal of the load balancer and values are HTTP or HTTPS."
+  type        = string
+  default     = null
+}
+
+variable "health_check_path" {
+  description = "health_check_path of the load balancer and values are HTTP or HTTPS."
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_port" {
+  description = "health_check_port of the load balancer and values are HTTP or HTTPS."
+  type        = number
+  default     = 80
+}
+variable "protocol_version" {
+  description = "Protocal of the load balancer and values are HTTP or HTTPS."
+  type        = string
+  default     = "HTTPS"
+}
+
+
