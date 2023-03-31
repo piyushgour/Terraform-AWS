c16cd86151240d80b44026298ae8bf3def8f01c8
Piyush Gour <piyush.gour@hitachivantara.com>
2023-03-31 10:34:27 +0530

    updated readme

--- 9.Modules/new-alb/module/main.tf
+++ 9.Modules/new-alb/module/main.tf
@@ -382,9 +382,9 @@ resource "aws_lb_listener" "frontend_https" {
   )
 }
 
-resource "aws_lb_listener_certificate" "https_listener" {
-  count = local.create_lb ? length(var.extra_ssl_certs) : 0
+# resource "aws_lb_listener_certificate" "https_listener" {
+#   count = local.create_lb ? length(var.extra_ssl_certs) : 0
 
-  listener_arn    = aws_lb_listener.frontend_https[var.extra_ssl_certs[count.index]["https_listener_index"]].arn
-  certificate_arn = var.extra_ssl_certs[count.index]["certificate_arn"]
-}
+#   listener_arn    = aws_lb_listener.frontend_https[var.extra_ssl_certs[count.index]["https_listener_index"]].arn
+#   certificate_arn = var.extra_ssl_certs[count.index]["certificate_arn"]
+# }
